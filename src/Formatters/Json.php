a7b8acf482e807656593d1748e44c9b3607de45c
Cabyca <53539466+Cabyca@users.noreply.github.com>
2020-11-24 10:25:15 +0300

    Update Json.php

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -2,12 +2,7 @@
 
 namespace Differ\Formatters\Json;
 
-function render($astTree)
-{
-    return json($astTree);
-}
-
-function json(array $astTree)
+function render(array $astTree)
 {
     return json_encode($astTree);
 }
